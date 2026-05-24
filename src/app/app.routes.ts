3b6a7627c433778279226c1bc367afecee5441ff
Santys2004 <santiagosuarez727@gmail.com>
2026-05-24 15:10:31 -0500

    configuracion rutas y navar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { CampanasComponent } from './components/campanas/campanas.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'campanas', component: CampanasComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: '**', redirectTo: '' }
+];
