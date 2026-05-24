8308484ba48a9cdfd9fc45eea1bc03e79c6d1224
Santys2004 <santiagosuarez727@gmail.com>
2026-05-24 17:21:00 -0500

    app funcionando, SSR desactivado

--- src/app/components/campanas/campanas.component.ts
+++ src/app/components/campanas/campanas.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ApiService } from '../../services/api';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-campanas',
--- src/app/services/api.spec.ts
+++ src/app/services/api.spec.ts
@@ -1,16 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { ApiService } from './api.service';
+import { provideHttpClient } from '@angular/common/http';
 
-import { Api } from './api';
-
-describe('Api', () => {
-  let service: Api;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Api);
-  });
+describe('ApiService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [provideHttpClient()]
+  }));
 
   it('should be created', () => {
+    const service: ApiService = TestBed.inject(ApiService);
     expect(service).toBeTruthy();
   });
 });
